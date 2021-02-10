1d6d063119cfad502618f553a879b6cb7d0b67c2
Marco Jansen <marco.jansen@gmx.us>
2021-02-10 18:34:45 +0800

    get, post and delete implemented into routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,64 +1,67 @@
 const db = require('../db/db.json');
 const router = require('express').Router();
+const uniqid = require('uniqid');
+const fs = require("fs")
+const path = require('path');
+const { raw } = require('express');
 
 //bonus consider errors
+const DB_PATH = path.join(__dirname, '..', 'db', 'db.json');
 
 //get notes
 router.get('/notes', (req, res) => {
-  // res.json(notes);
-});
+  let rawdata = fs.readFileSync(DB_PATH, 'utf-8');
+  let db = JSON.parse(rawdata)
+  res.json(db);
+  }
+);
 
 //post notes
-router.post('/notes/', (req, res) => {
-  //read fsread db
-  //create a new id and set a new id
-  //push note posted into the array
-  //fswrite
-  //if succesful return a boolean
+router.post('/notes', (req, res) => {
+
+  let rawdata = fs.readFileSync(DB_PATH, 'utf-8');
   
+  let notes;
+  try {
+    notes = JSON.parse(rawdata) || [];
+  } catch (err) {
+    console.error(err);
+    notes = [];
+  }
+  let newId = uniqid();
+  const note = {
+    id: newId,
+    title: req.body.title,
+    text: req.body.text,
+  }
+  notes.push(note);
+  
+  let output = JSON.stringify(notes, null, 2);
+  
+  fs.writeFileSync(DB_PATH, output)
+
+  res.send(true);
 });
 
 //delete notes
 
 router.delete('/notes/:id', (req, res) => {
-  //grab the id for the note to delete comes from params
-  //read file
-  // find the object with id suggest using filter
-  // remove that object from the array which is our db
-  // return a boolean
-  
+  const idToDelete = req.params;
+  console.log(idToDelete);
+  let rawdata = fs.readFileSync(DB_PATH, 'utf-8');
+  let notes;
+  try {
+    notes = JSON.parse(rawdata) || [];
+  } catch (err) {
+    console.error(err);
+    notes = [];
+  }
+  console.log(notes);
+  let newNotes = notes.filter(sort => sort.id !== idToDelete.id);
+  let output = JSON.stringify(newNotes, null, 2);
+  fs.writeFileSync(DB_PATH, output)
+  res.send(true);
 });
 
 module.exports = router;
 
-
-//old code
-
-// module.exports = (app) => {
-
-// 	app.get('/api/notes/:character', (req, res) => {
-//     const chosen = req.params.notes;
-  
-//     console.log(chosen);
-
-//     for (let i = 0; i < notes.length; i++) {
-//       if (chosen === notes[i].routeName) {
-//         return res.json(characters[i]);
-//       }
-//     }
-  
-//     return res.json(false);
-//   });
-  
-//     app.post('/api/notes', (req, res) => {
-//     const newNote = req.body;
-
-//     newNote.routeName = newNote.name.replace(/\s+/g, '').toLowerCase();
-//     console.log(newNote);
-  
-//     notes.push(newNote);
-//     res.json(newNote);
-//   });
-
-
-// };
--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const apiRoutes = require('./routes/apiRoutes')
 const app = express();
 
 const PORT = process.env.PORT || 8080;
-// const notes = {};
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,28 +20,5 @@ app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
 });
 
-// app.get('/api/notes/:character', (req, res) => {
-//     const chosen = req.params.notes;
-  
-//     console.log(chosen);
-
-//     for (let i = 0; i < notes.length; i++) {
-//       if (chosen === notes[i].routeName) {
-//         return res.json(characters[i]);
-//       }
-//     }
-  
-//     return res.json(false);
-//   });
-  
-//   app.post('/api/notes', (req, res) => {
-//     const newNote = req.body;
-
-//     newNote.routeName = newNote.name.replace(/\s+/g, '').toLowerCase();
-//     console.log(newNote);
-  
-//     notes.push(newNote);
-//     res.json(newNote);
-//   });
 
 
